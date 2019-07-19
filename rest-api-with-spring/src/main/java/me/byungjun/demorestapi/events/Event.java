33a8d8c1af032e97f8b94f60fca7dce419c8780a
Byungjun Son <saint2030@naver.com>
2019-07-19 23:43:05 +0900

    fail on unknown properteis

--- rest-api-with-spring/src/main/java/me/byungjun/demorestapi/events/Event.java
+++ rest-api-with-spring/src/main/java/me/byungjun/demorestapi/events/Event.java
@@ -33,6 +33,6 @@ public class Event {
   private boolean offline;
   private boolean free;
   @Enumerated(EnumType.STRING)
-  private EventStatus eventStatus;
+  private EventStatus eventStatus = EventStatus.DRAFT;
 
 }
--- rest-api-with-spring/src/test/java/me/byungjun/demorestapi/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/byungjun/demorestapi/events/EventControllerTests.java
@@ -33,6 +33,34 @@ public class EventControllerTests {
 
   @Test
   public void createEvent() throws Exception {
+    EventDto event = EventDto.builder()
+        .name("Spring")
+        .description("REST Development with Spring")
+        .beginEnrollmentDateTime(LocalDateTime.of(2019, 07, 19, 10, 10))
+        .closeEnrollmentDateTime(LocalDateTime.of(2019, 07, 20, 10, 10))
+        .endEventDateTime(LocalDateTime.of(2019, 07, 25, 10, 00))
+        .basePrice(100)
+        .maxPrice(200)
+        .limitOfEnrollment(100)
+        .location("강남역 D2 스타텁 팩토리")
+        .build();
+
+    mockMvc.perform(post("/api/events/")
+              .contentType(MediaType.APPLICATION_JSON_UTF8)
+              .accept(MediaTypes.HAL_JSON)
+              .content(objectMapper.writeValueAsString(event)))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("id").exists())
+        .andExpect(header().exists(HttpHeaders.LOCATION))
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+        .andExpect(jsonPath("id").value(Matchers.not(100)))
+        .andExpect(jsonPath("free").value(Matchers.not(true)))
+        .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)));
+  }
+
+  @Test
+  public void createEvent_Bad_Request() throws Exception {
     Event event = Event.builder()
         .id(100)
         .name("Spring")
@@ -50,17 +78,11 @@ public class EventControllerTests {
         .build();
 
     mockMvc.perform(post("/api/events/")
-              .contentType(MediaType.APPLICATION_JSON_UTF8)
-              .accept(MediaTypes.HAL_JSON)
-              .content(objectMapper.writeValueAsString(event)))
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.HAL_JSON)
+        .content(objectMapper.writeValueAsString(event)))
         .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("id").exists())
-        .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-        .andExpect(jsonPath("id").value(Matchers.not(100)))
-        .andExpect(jsonPath("free").value(Matchers.not(true)))
-        .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)));
+        .andExpect(status().isBadRequest());
   }
 
 }
