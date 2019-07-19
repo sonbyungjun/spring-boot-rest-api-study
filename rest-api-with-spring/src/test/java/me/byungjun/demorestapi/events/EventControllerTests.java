a5b81f5023ee2bdecf5f7064af9da87c6638d668
Byungjun Son <saint2030@naver.com>
2019-07-19 22:54:01 +0900

    ㅔplishing

--- rest-api-with-spring/src/test/java/me/byungjun/demorestapi/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/byungjun/demorestapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package me.byungjun.demorestapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -44,7 +46,7 @@ public class EventControllerTests {
         .limitOfEnrollment(100)
         .location("강남역 D2 스타텁 팩토리")
         .build();
-    
+
     event.setId(10);
     Mockito.when(eventRepository.save(event)).thenReturn(event);
 
@@ -54,7 +56,9 @@ public class EventControllerTests {
               .content(objectMapper.writeValueAsString(event)))
         .andDo(print())
         .andExpect(status().isCreated())
-        .andExpect(jsonPath("id").exists());
+        .andExpect(jsonPath("id").exists())
+        .andExpect(header().exists(HttpHeaders.LOCATION))
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE));
   }
 
 }
